c3bc6b7c8132bf42f1081d35148fec584629b6ad
Vinz <intensity67@gmail.com>
2022-10-31 17:39:20 +0800

    finished tutorial- added pause and play music according to dropdown

--- popout.js
+++ popout.js
@@ -1,1 +1,18 @@
+document.querySelector('button.play').addEventListener('click', function(){
 
+    var SelTrack = document.querySelector('select').value;
+
+    chrome.runtime.sendMessage({name: "playTrack", track: SelTrack});
+
+});
+
+document.querySelector('button.pause').addEventListener('click', function(){
+
+    // var audioEle = document.querySelector('.audio-element');
+
+    // audioEle.pause();
+
+    chrome.runtime.sendMessage({name: "pauseTrack"});
+
+
+});
